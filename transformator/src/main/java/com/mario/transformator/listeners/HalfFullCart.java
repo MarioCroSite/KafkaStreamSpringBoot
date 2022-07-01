6d83ad5a46dbf284bbb7478e2e0c9e78ee233e94
MarioCroSite <crositecom@gmail.com>
2022-07-01 02:46:19 +0200

    Added art

---
+++ transformator/src/main/java/com/mario/transformator/listeners/HalfFullCart.java
@@ -0,0 +1,24 @@
+package com.mario.transformator.listeners;
+
+import com.mario.events.OrderFullEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HalfFullCart {
+
+    private static final Logger logger = LoggerFactory.getLogger(FullMiniCart.class);
+
+    @KafkaListener(
+            topics = {"${com.mario.kafka.half-full-cart}"},
+            groupId = "${com.mario.kafka.consumer-group-id}",
+            autoStartup = "true",
+            containerFactory = "listenerFactory")
+    public void receiveToTopic(OrderFullEvent orderFullEvent) {
+        logger.info("[HALF-FULL-CART]");
+        logger.info(orderFullEvent.toString());
+    }
+
+}
